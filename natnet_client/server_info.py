44daacd90681abe7282cdc838085ae456884c180
Alex Oldemeier (work) <alexander.oldemeier@tum.de>
2024-06-24 10:51:08 +0200

    Read clock frequency from server info

--- natnet_client/server_info.py
+++ natnet_client/server_info.py
@@ -8,11 +8,14 @@ from .version import Version
 class ServerInfo(PacketComponent, NamedTuple("ServerInfoFields", (
         ("application_name", str),
         ("server_version", Version),
-        ("nat_net_protocol_version", Version)))):
+        ("nat_net_protocol_version", Version),
+        ("high_res_clock_frequency", int)))):
 
     @classmethod
     def read_from_buffer(cls, buffer: PacketBuffer, protocol_version: Version) -> "ServerInfo":
         application_name = buffer.read_string(256, static_length=True)
         server_version = Version(*buffer.read("BBBB"))
         nat_net_protocol_version = Version(*buffer.read("BBBB"))
-        return cls(application_name, server_version, nat_net_protocol_version)
+        if nat_net_protocol_version >= Version(4):
+            high_res_clock_frequency = buffer.read_uint64()
+        return cls(application_name, server_version, nat_net_protocol_version, high_res_clock_frequency)
